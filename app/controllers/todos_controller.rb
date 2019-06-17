84d09d80c2f7f856853d4586bbe4441996307b7b
pablo <pabloperezpastene@yahoo.es>
2019-06-17 16:38:48 -0400

    Tercer commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,13 @@
 class TodosController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @task = Task.find(params[:task_id])
+    @todo = Todo.new(task: @task, user: current_user)
+    if @todo.save
+      redirect_to tasks_path, notice: 'La tarea ha sido agregada'
+    else
+      redirect_to tasks_path, alert: 'La tarea no ha podido ser agregada'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get 'todos/create'
-  resources :tasks
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks do
 
+  resources :todos, only: :create
+
+  end
+
+
+
+
+  devise_for :users
   root to: "tasks#index"
 end
