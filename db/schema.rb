11e25ed9f6da1282936ec5736e87f6e379f91312
pablo <pabloperezpastene@yahoo.es>
2019-06-17 16:21:22 -0400

    segundo commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_150221) do
+ActiveRecord::Schema.define(version: 2019_06_17_151159) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2019_06_17_150221) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "task_id"
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_todos_on_task_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Todo.destroy_all
+User.destroy_all
+Task.destroy_all
+Task.create([
+  {
+    name: 'Comer Asado',
+    photo: 'https://www.baenegocios.com/__export/1525988453319/sites/cronica/img/2018/05/10/10-05-2018asado_entre_amigos.jpg_541755376.jpg'
+  },
+  {
+    name: 'Comer Empanada de Pino',
+    photo: 'https://www.gourmet.cl/wp-content/uploads/2011/04/empanada-e1314790821899.jpg'
+  },
+  {
+    name: 'Comer Anticucho',
+    photo: 'https://www.gourmet.cl/wp-content/uploads/2012/07/anticuchos-de-sobre-costilla.jpg'
+  },
+  {
+    name: 'Encumbrar Volantin',
+    photo: 'https://chrieseli.files.wordpress.com/2008/10/volantin.jpg'
+  },
+  {
+    name: 'Beber Terremoto',
+    photo: 'https://media.cnnchile.com/2018/09/terremoto-02-700x430.jpg'
+  },
+  {
+    name: 'Tirarse unas Payas',
+    photo: 'https://www.cooperativa.cl/noticias/site/artic/20140124/imag/foto_0000000420140124221504.jpg'
+  },
+  {
+    name: 'Comer Choripan',
+    photo: 'http://www.gamba.cl/wp-content/uploads/2015/10/Pi%C3%B1era-comiendo-chorip%C3%A1n.jpg'
+  },
+  {
+    name: 'Jugar Al Trompo',
+    photo: 'https://www.thisischile.cl/wp-content/uploads/2017/05/trombodef.png'
+  },
+  {
+    name: 'Jugar Al Emboque',
+    photo: 'https://www.aprenderjuntos.cl/wp-content/uploads/2016/09/emboque-portada.jpg'
+  },
+
+
+  ])
